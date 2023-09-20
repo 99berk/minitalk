915dc892345fd1b65efa50096089676e5375a735
Berhan Berk Akgun <bakgun@k2m06s01.42kocaeli.com.tr>
2023-09-20 13:37:21 +0300

    min4.f

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: bakgun <bakgun@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 13:40:41 by bakgun            #+#    #+#             */
-/*   Updated: 2023/09/10 17:24:34 by bakgun           ###   ########.fr       */
+/*   Updated: 2023/09/13 14:37:09 by bakgun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,51 @@
 #include <signal.h>
 #include <unistd.h>
 
+int	get_client_pid(int i)
+{
+	static int	bit2 = 31;
+	static int	ascii_pid = 0;
+	int			pid_tmp;
+
+	pid_tmp = 0;
+	ascii_pid += (i << bit2);
+	if (bit2 == 0)
+	{
+		bit2 = 31;
+		pid_tmp = ascii_pid;
+		ascii_pid = 0;
+		return (pid_tmp);
+	}
+	else
+		bit2--;
+	return (pid_tmp);
+}
+
 void	bit_print(int i)
 {
 	static int	bit = 7;
 	static int	ascii = 0;
+	static int	client_pid = 0;
 
-	ascii += (i << bit);
-	if (bit == 0)
-	{
-		ft_printf("%c", ascii);
-		bit = 7;
-		ascii = 0;
-	}
+	if (client_pid == 0)
+		client_pid = get_client_pid(i);
 	else
-		bit--;
+	{
+		ascii += (i << bit);
+		if (bit == 0)
+		{
+			ft_printf("%c", ascii);
+			if (ascii == '\n')
+			{
+				kill(client_pid, SIGUSR1);
+				client_pid = 0;
+			}
+			ascii = 0;
+			bit = 7;
+		}
+		else
+			bit--;
+	}
 }
 
 void	sig_control(int sig)
@@ -44,8 +75,6 @@ int	main(void)
 	signal(SIGUSR1, sig_control);
 	signal(SIGUSR2, sig_control);
 	while (1)
-	{
 		pause();
-	}
 	return (0);
 }
